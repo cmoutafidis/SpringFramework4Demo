a388ff373519e960e140f048eaaa3728cd3b8157
Charis Moutafidis <charis.moutafidis@hapimag.com>
2021-09-02 14:47:12 +0300

    Hello World example

--- src/main/java/com/cmoutafidis/springframework4demo/SpringFramework4DemoApplication.java
+++ src/main/java/com/cmoutafidis/springframework4demo/SpringFramework4DemoApplication.java
@@ -2,12 +2,15 @@ package com.cmoutafidis.springframework4demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringFramework4DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringFramework4DemoApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(SpringFramework4DemoApplication.class, args);
+        HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
+        helloWorld.sayHello();
     }
 
 }
