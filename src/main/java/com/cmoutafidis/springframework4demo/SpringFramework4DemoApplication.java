da0f4e3cce00e6064e82d167ae012e57a4a8154a
Charis Moutafidis <charis.moutafidis@hapimag.com>
2021-09-02 14:39:00 +0300

    Project Init

---
+++ src/main/java/com/cmoutafidis/springframework4demo/SpringFramework4DemoApplication.java
@@ -0,0 +1,13 @@
+package com.cmoutafidis.springframework4demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFramework4DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFramework4DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cmoutafidis/springframework4demo/SpringFramework4DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmoutafidis.springframework4demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFramework4DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
